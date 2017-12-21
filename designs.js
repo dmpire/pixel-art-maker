aa79c1852390b71e524c8e938dd95d008842e70d
Daniella Mato <dmpire2@gmail.com>
2017-12-21 21:32:38 +0100

    FIX: fix indentation and spacing issues as well as change naming conventions to camel case

--- designs.js
+++ designs.js
@@ -1,36 +1,27 @@
 $('document').ready(function(){
+	$('#sizePicker').submit(function makeGrid(e) {
+		e.preventDefault();
+		let column = $('#inputHeight').val();
+		let row = $('#inputWidth').val();
+		let pixelCanvas = $('#pixelCanvas');
+		let tableTag = pixelCanvas.html('<table class="grid-cells"></table>');
 
-    $('#sizePicker').submit(function makeGrid(e) {
-  e.preventDefault();
+		for(var x = 0;x < row; x++){
+			tableTag.append('<tr>');
 
-		// Select size input
-		let column = $('#input_height').val();
-		let row = $('#input_width').val();
-		// When size is submitted by the user, call makeGrid()
-		let pixel_canvas = $('#pixel_canvas');
-
-		var tableTag = pixel_canvas.html('<table class="grid-cells"></table>');
-			for(var x = 0;x < row; x++){
-					tableTag.append('<tr>');
-
-				for(var y=0; y < column; y++){
-					$(tableTag).append('<td></td>');
-				}
-
-				tableTag.append('</tr>');
+			for(var y=0; y < column; y++){
+				$(tableTag).append('<td></td>');
 			}
 
+			tableTag.append('</tr>');
+		}
 	});
 
-
-$(colorPicker).on('change',function(){
-         // Select color input
-    let colorPicker = $('#colorPicker').val();
-          $('table').on('click', 'td', function() {
-              $( this ).css('background-color',colorPicker);
-          });
-        });
-
-
+	$(colorPicker).on('change',function(){
+		let colorPicker = $('#colorPicker').val();
+		$('table').on('click', 'td', function() {
+			$( this ).css('background-color',colorPicker);
+		});
+	});
 });
 
