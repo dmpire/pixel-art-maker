40aabab6d9c6de14a8b334feac2b0e9025edff18
Daniella Mato <dmpire2@gmail.com>
2018-01-04 08:09:47 +0100

    Make black the default color, make it partially responsive, make loops according to es6 standard

--- designs.js
+++ designs.js
@@ -3,20 +3,22 @@ $('document').ready(function(){
 		e.preventDefault();
 		let column = $('#inputHeight').val();
 		let row = $('#inputWidth').val();
-		let pixelCanvas = $('#pixelCanvas');
+		const pixelCanvas = $('#pixelCanvas');
 		let tableTag = pixelCanvas.html('<table class="grid-cells"></table>');
 
-		for(var x = 0;x < row; x++){
-			tableTag.append('<tr>');
+		for(let x = 0;x < row; x++){
+			let tr = tableTag.append('<tr>');
 
-			for(var y=0; y < column; y++){
-				$(tableTag).append('<td></td>');
+			for(let y=0; y < column; y++){
+				$(tr).append('<td></td>');
 			}
 
-			tableTag.append('</tr>');
 		}
 	});
 
+	$('table').on('click', 'td', function() {
+		$( this ).css('background-color','#000');
+	});
 	$(colorPicker).on('change',function(){
 		let colorPicker = $('#colorPicker').val();
 		$('table').on('click', 'td', function() {
